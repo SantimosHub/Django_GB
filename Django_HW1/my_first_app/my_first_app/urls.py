4f040bde4f551cd3984afaefea2459d1715cd259
«Santimos» <stosoleg@gmail.com>
2024-02-08 14:16:48 +0300

    Last edit

--- Django_HW1/my_first_app/my_first_app/urls.py
+++ Django_HW1/my_first_app/my_first_app/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for my_first_app project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
--- Django_HW1/my_first_app/my_first_app/wsgi.py
+++ Django_HW1/my_first_app/my_first_app/wsgi.py
@@ -4,6 +4,7 @@ WSGI config for my_first_app project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
